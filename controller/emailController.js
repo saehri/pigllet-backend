ea821506ae8714e85afa76e1f36fec0d75223145
Saepul Bahri <bahreesaepul1@gmail.com>
2024-12-29 10:48:50 +0700

    migrated the user routes to supabase

--- controller/emailController.js
+++ controller/emailController.js
@@ -1,107 +1,105 @@
-const {PrismaClient} = require('@prisma/client');
+const supabase = require('../lib/supabaseClient');
 const emailtl = require('../lib/emailUtil');
 const genStr = require('../lib/generateRandomString');
 
-const prisma = new PrismaClient();
-
 // @func get all entries of verification link
 async function getAllvLinks(req, res) {
-  try {
-    const vLinks = await prisma.emailVerification.findMany();
+	try {
+		const vLinks = await prisma.emailVerification.findMany();
 
-    res.json({message: '', data: vLinks});
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
+		res.json({message: '', data: vLinks});
+	} catch (error) {
+		res.status(500).json({message: error.message});
+	}
 }
 
 // @func - void send the user verification link
 async function sendUserVerificationEmail(req, res) {
-  try {
-    const {userId, email} = req.params;
-    const verifId = genStr(35);
+	try {
+		const {userId, email} = req.params;
+		const verifId = genStr(35);
 
-    // create the verification link
-    await prisma.emailVerification.create({
-      data: {
-        userId: Number(userId),
-        verificationId: verifId,
-        createdAt: new Date().toISOString(),
-      },
-    });
+		// create the verification link
+		await prisma.emailVerification.create({
+			data: {
+				userId: Number(userId),
+				verificationId: verifId,
+				createdAt: new Date().toISOString(),
+			},
+		});
 
-    // send email verification to user
-    await emailtl.sendEmailAdressVerificationEmail(
-      email,
-      `api/email/verif/${verifId.trim()}`
-    );
+		// send email verification to user
+		await emailtl.sendEmailAdressVerificationEmail(
+			email,
+			`api/email/verif/${verifId.trim()}`
+		);
 
-    res.json({
-      message: 'Email address verification link created successfully',
-    });
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
+		res.json({
+			message: 'Email address verification link created successfully',
+		});
+	} catch (error) {
+		res.status(500).json({message: error.message});
+	}
 }
 
 // @func void - handle the process of email address verification and verification link removal
 async function verifyUserEmail(req, res) {
-  try {
-    const {verifId} = req.params;
+	try {
+		const {verifId} = req.params;
 
-    const vLink = await prisma.emailVerification.findFirst({
-      where: {
-        verificationId: verifId,
-      },
-    });
+		const vLink = await prisma.emailVerification.findFirst({
+			where: {
+				verificationId: verifId,
+			},
+		});
 
-    if (vLink === null) {
-      return res.status(404).json({message: 'Invalid email verification link'});
-    }
+		if (vLink === null) {
+			return res.status(404).json({message: 'Invalid email verification link'});
+		}
 
-    // add a function that make the verification link invalid (and removed) if its older than 5 minutes after its creations
-    const date = new Date(vLink.createdAt);
-    const differenceInMs = date - new Date();
+		// add a function that make the verification link invalid (and removed) if its older than 5 minutes after its creations
+		const date = new Date(vLink.createdAt);
+		const differenceInMs = date - new Date();
 
-    const differenceInMinutes = Math.abs(differenceInMs) / (1000 * 60);
-    if (differenceInMinutes > 5) {
-      // remove all verification link of the user
-      await prisma.emailVerification.deleteMany({
-        where: {
-          userId: vLink.userId,
-        },
-      });
+		const differenceInMinutes = Math.abs(differenceInMs) / (1000 * 60);
+		if (differenceInMinutes > 5) {
+			// remove all verification link of the user
+			await prisma.emailVerification.deleteMany({
+				where: {
+					userId: vLink.userId,
+				},
+			});
 
-      return res.json({
-        message: 'Your verification link is invalid, please create a new one',
-      });
-    }
+			return res.json({
+				message: 'Your verification link is invalid, please create a new one',
+			});
+		}
 
-    // update he user email status to verified
-    const user = await prisma.user.update({
-      where: {
-        id: vLink.userId,
-      },
-      data: {
-        emailVerified: 1,
-      },
-    });
+		// update he user email status to verified
+		const user = await prisma.user.update({
+			where: {
+				id: vLink.userId,
+			},
+			data: {
+				emailVerified: 1,
+			},
+		});
 
-    // remove all verification link for the user
-    await prisma.emailVerification.deleteMany({
-      where: {
-        userId: vLink.userId,
-      },
-    });
+		// remove all verification link for the user
+		await prisma.emailVerification.deleteMany({
+			where: {
+				userId: vLink.userId,
+			},
+		});
 
-    res.json({message: 'Email verified successfully'});
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
+		res.json({message: 'Email verified successfully'});
+	} catch (error) {
+		res.status(500).json({message: error.message});
+	}
 }
 
 module.exports = {
-  sendUserVerificationEmail,
-  verifyUserEmail,
-  getAllvLinks,
+	sendUserVerificationEmail,
+	verifyUserEmail,
+	getAllvLinks,
 };
--- controller/storageController.js
+++ controller/storageController.js
@@ -1,93 +1,91 @@
-const {PrismaClient} = require('@prisma/client');
-
-const prisma = new PrismaClient();
+const supabase = require('../lib/supabaseClient');
 
 async function getUserData(req, res) {
-  try {
-    const {userId} = req.params;
-    const data = await prisma.storage.findUnique({
-      where: {
-        userId: Number(userId),
-      },
-      include: {user: true},
-    });
+	try {
+		const {userId} = req.params;
+		const data = await prisma.storage.findUnique({
+			where: {
+				userId: Number(userId),
+			},
+			include: {user: true},
+		});
 
-    if (!data) {
-      return res.status(404).json({message: 'No stored data was found'});
-    }
+		if (!data) {
+			return res.status(404).json({message: 'No stored data was found'});
+		}
 
-    res.json({message: '', data});
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
+		res.json({message: '', data});
+	} catch (error) {
+		res.status(500).json({message: error.message});
+	}
 }
 
 async function getData(req, res) {
-  try {
-    const data = await prisma.storage.findMany();
+	try {
+		const data = await prisma.storage.findMany();
 
-    res.json({message: '', data});
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
+		res.json({message: '', data});
+	} catch (error) {
+		res.status(500).json({message: error.message});
+	}
 }
 
 async function storeData(req, res) {
-  try {
-    const {data, userId} = req.body;
+	try {
+		const {data, userId} = req.body;
 
-    const storage = await prisma.storage.create({
-      data: {
-        data: data,
-        userId,
-        createdAt: new Date().toISOString(),
-      },
-    });
+		const storage = await prisma.storage.create({
+			data: {
+				data: data,
+				userId,
+				createdAt: new Date().toISOString(),
+			},
+		});
 
-    res.json({message: 'Successfully stored your data', data: storage});
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
+		res.json({message: 'Successfully stored your data', data: storage});
+	} catch (error) {
+		res.status(500).json({message: error.message});
+	}
 }
 
 async function updateStorage(req, res) {
-  try {
-    const {userId} = req.params;
-    const data = req.body;
+	try {
+		const {userId} = req.params;
+		const data = req.body;
 
-    const storage = await prisma.storage.update({
-      where: {userId: Number(userId)},
-      data: {
-        ...data,
-        updatedAt: new Date().toISOString(),
-      },
-    });
+		const storage = await prisma.storage.update({
+			where: {userId: Number(userId)},
+			data: {
+				...data,
+				updatedAt: new Date().toISOString(),
+			},
+		});
 
-    res.json({message: 'Storage updated', data: {...storage}});
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
+		res.json({message: 'Storage updated', data: {...storage}});
+	} catch (error) {
+		res.status(500).json({message: error.message});
+	}
 }
 
 async function deleteStorage(req, res) {
-  try {
-    const {userId} = req.params;
-    await prisma.storage.delete({
-      where: {
-        userId: Number(userId),
-      },
-    });
+	try {
+		const {userId} = req.params;
+		await prisma.storage.delete({
+			where: {
+				userId: Number(userId),
+			},
+		});
 
-    res.json({message: 'Storage removed successfully'});
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
+		res.json({message: 'Storage removed successfully'});
+	} catch (error) {
+		res.status(500).json({message: error.message});
+	}
 }
 
 module.exports = {
-  storeData,
-  getData,
-  getUserData,
-  updateStorage,
-  deleteStorage,
+	storeData,
+	getData,
+	getUserData,
+	updateStorage,
+	deleteStorage,
 };
